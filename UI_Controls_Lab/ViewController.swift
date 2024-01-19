6ddfb07bb66717e98ee29d7ea4d18a8ee0df9857
Srinu Rachakonda <srinurachakonda126@gmail.com>
2024-01-19 17:47:03 -0500

    UI-Controls

--- UI_Controls_Lab/ViewController.swift
+++ UI_Controls_Lab/ViewController.swift
@@ -8,7 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var screenNumber: Int = 0;
+    var step: Int = 1;
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBAction func Step(_ sender: Any) {
+        step = 2;
+    }
+    
+    @IBAction func Reset(_ sender: Any) {
+        screenNumber = 0;
+        Label.text = String(screenNumber);
+    }
+    @IBAction func Increment(_ sender: Any) {
+        if(step==2){
+            screenNumber += 2;
+        } else {
+            screenNumber += 1;
+        }
+        Label.text = String(screenNumber);
+    }
+    
+    @IBAction func Decrement(_ sender: Any) {
+        if(step == 2){
+            screenNumber -= 2;
+        } else {
+            screenNumber -= 1;
+        }
+        Label.text = String(screenNumber);
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
