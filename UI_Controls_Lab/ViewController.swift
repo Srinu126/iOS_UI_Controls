bec4e08faca3bb0be86d46b7c6fad81dcc80974c
Srinu <srinurachakonda126@gmail.com>
2024-01-19 14:49:14 -0500

    Initial Commit

---
+++ UI_Controls_Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI_Controls_Lab
+//
+//  Created by user238295 on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
